ea8a2d04e62c594f59cc80479cc11e091e04bd0f
Tony Dehnke <tdehnke@me.com>
2018-12-19 14:55:35 +0700

    Stock Listing & Delete functions

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -5,10 +5,19 @@ class UserStocksController < ApplicationController
     if stock.blank?
       stock = Stock.new_from_lookup(params[:stock_ticker])
       stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:success] = "Stock #{@user_stock.stock.name} was succesfully added to porfolio"
+    redirect_to my_portfolio_path
   end
-  @user_stock = UserStock.create(user: current_user, stock: stock)
-  flash[:success] = "Stock #{@user_stock.stock.name} was succesfully added to porfolio"
-  redirect_to my_portfolio_path
-end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    @user_stock.destroy
+    flash[:notice] = "Stock was successfuly removed from your list"
+    redirect_to my_portfolio_path
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
+  
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
